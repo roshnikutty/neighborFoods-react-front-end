3e45f89cae59a2e699294868001c3b856e88eea3
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-08-18 20:55:24 -0400

    date format updated in meals page

--- src/Meals/Meals.js
+++ src/Meals/Meals.js
@@ -15,13 +15,19 @@ class Meals extends React.Component {
 
         if (this.props.existingMeals) {
             allExistingMeals = this.props.existingMeals.map((meal) => {
+                //converting iso 8601 date to string
+                let dString = `${meal.sell_date}`;
+                //extracting month, date, year from iso 8601 format separated by hyphens
+                let dSplit = dString.replace(/\D/g," ").split(" ");
+                let date = (`${dSplit[1]}-${dSplit[2]}-${dSplit[0]}`);
+
                 return (
                     //key uses backend meal id (meal_id)
                     <li className="black-box meal-item" key={meal.meal_id}>
                         <strong>Seller's Name</strong> {meal.seller_name} <br />
                         <strong>Dish</strong> {meal.sell_dish}<br />
                         <strong>Cuisine</strong> {meal.sell_cuisine}<br />
-                        <strong>Date</strong> {meal.sell_date}<br />
+                        <strong>Date</strong> {date}<br />
                         <strong>Number of plates</strong> {meal.sell_plate_count}<br />
                         <strong>Price per plate</strong> ${meal.sell_plate_cost}<br />
                         <strong>Allergens</strong> {meal.sell_allergens}<br />
