8df0f5e3cc508c50bdb25677f58bb287a2a8c3b1
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-08-29 20:52:42 -0400

    working test, session expired page added

--- src/Sessionexpired.js
+++ src/Sessionexpired.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Login from './Login';
+import { Link } from 'react-router-dom';
+import { push } from 'react-router-redux';
+// import Login from './Login';
 import './App/app.css';
 
 
-const Sessionexpired = () => {
-    return(
+const Sessionexpired = (props) => {
+    return (
         <div>
+            <header>
+                <h1><Link to="/">NeighborFoods</Link></h1>
+            </header>
             <p className="black-box">You must log in to continue.</p>
-                <Login />
+            <p className="buttons-box">
+                <button className="landing-button session-button" onClick={props.gotoLogin} >Log In</button>
+                <button className="landing-button session-button" onClick={props.gotoSignup}>Sign Up</button>
+                {/*<Login />*/}
+            </p>
         </div>
     );
-
 }
+const mapDispatchToProps = (dispatch) => ({
+    gotoLogin: () => dispatch(push('/login')),
+    gotoSignup: () => dispatch(push('/signup'))
+})
 
-export default connect()(Sessionexpired);
+export default connect(null, mapDispatchToProps)(Sessionexpired);
