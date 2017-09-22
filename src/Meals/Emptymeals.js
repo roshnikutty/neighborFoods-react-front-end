35fb7557ed27cee60b2c3ad9048bd7aabc7ea741
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-09-22 09:46:47 -0400

    updated style for meal-area with no meals

--- src/Meals/Emptymeals.js
+++ src/Meals/Emptymeals.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
-// import { push } from 'react-router-redux';
 
 const Emptymeals = (props) => {
     return (
